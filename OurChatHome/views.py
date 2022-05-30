2644cca0644b50706250cd27b3b00e3fd54f154d
Dumitrache Valentin <dumitrache.edi.valentin@gmail.com>
2022-05-30 22:35:03 +0300

    initial commit

---
+++ OurChatHome/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+
+
+def frontpage(request):
+    return render(request, 'OurChatHome/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+        
+    else:
+        form = SignUpForm()
+
+    return render(request, 'OurChatHome/signup.html', {'form': form})
+
+
