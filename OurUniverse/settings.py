71c66bf405684d1d3a54c92cca05abe4ced8b529
Dumitrache Valentin <dumitrache.edi.valentin@gmail.com>
2022-05-30 23:07:37 +0300

    allow host for heroku modified in setings

--- OurUniverse/settings.py
+++ OurUniverse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9@s^)g#eiz#)r7k^ycxd#lajp_trj(wm*3c(&79=!stzi#l$nw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.4']
+ALLOWED_HOSTS = ['ourverse.herokuapp.com']
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/rooms/'
